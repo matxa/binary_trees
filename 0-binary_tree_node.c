2bfda49929417bb71189c4837e1ce396e2b7989f
matxa <42968545+matxa@users.noreply.github.com>
2020-07-20 23:12:51 -0400

    create node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,22 +1,24 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node -  a function that creates a binary tree node
-* @parent: parent
-* @value: value
-* Return: 
-*/
+ * binary_tree_node - a function that creates a binary tree node
+ * @parent: parent
+ * @value: value
+ * Return: new_leaf
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_leaf;
+	binary_tree_t *new_leaf;
 
-    new_leaf = malloc(sizeof(binary_tree_t));
-    if (new_leaf == NULL)
-    {
-        return (NULL);
-    }
-    new_leaf->n = value;
-    parent = new_leaf;
-    return (parent);
+	new_leaf = malloc(sizeof(binary_tree_t));
+	if (new_leaf == NULL)
+	{
+		return (NULL);
+	}
+	new_leaf->n = value;
+	new_leaf->parent = parent;
+	new_leaf->left = NULL;
+	new_leaf->right = NULL;
+	return (new_leaf);
 }
