da9c16aad9cd88cf2d0d37b687eef04e35dca948
matxa <42968545+matxa@users.noreply.github.com>
2020-07-22 13:21:17 -0400

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,15 +8,11 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count_leaves = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		count_leaves = count_leaves + 1;
-	if (tree->right)
-		count_leaves = count_leaves + 1;
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	return (count_leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
